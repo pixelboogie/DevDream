30b35b433bacaab5dbe6e2d6623a6eddb5b7c880
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-08-05 14:38:40 -0500

    4

--- index.js
+++ index.js
@@ -12,20 +12,10 @@ export const openai = new OpenAI({
     dangerouslyAllowBrowser: true
 })
 
-// const location = await getLocation();
-// const weather = await getCurrentWeather();
-/**
- * Goal - build an agent that can answer any questions that might require knowledge about my current location and the current weather at my location.
- */
-
-/**
- PLAN:
- 1. Design a well-written ReAct prompt
- 2. Build a loop for my agent to run in.
- 3. Parse any actions that the LLM determines are necessary
- 4. End condition - final Answer is given
- 
- */
+const availableFunctions = {
+    getCurrentWeather,
+    getLocation
+}
 
 const systemPrompt = `
 You cycle through Thought, Action, PAUSE, Observation. At the end of the loop you output a final Answer. Your final answer should be highly specific to the observations you have from running
@@ -64,48 +54,45 @@ You then output:
 Answer: <Suggested activities based on sunny weather that are highly specific to New York City and surrounding areas.>
 `
 
-/**
- * Challenge: Set up the function
- * 1. Create a function called `agent` that takes a `query` as a parameter
- * 2. Create a messages array that follows the pattern openai expects for 
- *    its chat completions endpoint. The first message should be the system
- *    prompt we wrote above, and the second message should be the query 
- *    from the user found in the `agent` function parameter.
- * 3. Move the code below inside the function (and uncomment it)
- * 4. Call the function with a string query of any kind and see what gets returned.
- */
-
 async function agent(query){
-    const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
+        const messages = [
                 { role: "system", content: systemPrompt },
                 { role: "user", content: query }
         ]
-    })
-    
-        /**
-     * PLAN:
-     * 1. Split the string on the newline character \n
-     * 2. Search through the array of strings for one that has "Action:"
-     *      regex to use: 
-     *      const actionRegex = /^Action: (\w+): (.*)$/
-     * 3. Parse the action (function and parameter) from the string
-     * 4. Calling the function
-     * 5. Add an "Obversation" message with the results of the function call
-     */
-    
-       const responseText = response.choices[0].message.content
-       const responseLines = responseText.split("\n")
-            
-            
-        const actionRegex = /^Action: (\w+): (.*)$/
-        const foundActionStr = responseLines.find(str => actionRegex.test(str))
-        const actions = actionRegex.exec(foundActionStr)
-        console.log(actions)
-      
         
+        const MAX_ITERATIONS = 5;
+        const actionRegex = /^Action: (\w+): (.*)$/
+        
+        for(let i=0; i<=MAX_ITERATIONS; i++){
+            console.log(`Iteration #${i + 1}`)
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages
+        })
+        
+        const responseText = response.choices[0].message.content
+        console.log(responseText)
+        messages.push({ role: "assistant", content: responseText })
+        const responseLines = responseText.split("\n")
+                
+            const foundActionStr = responseLines.find(str => actionRegex.test(str))
+            
+            if(foundActionStr){
+                const actions = actionRegex["exec"](foundActionStr)
+                const [_, action, actionArg] = actions
+                
+                if(!availableFunctions.hasOwnProperty(action)) {
+                    throw new Error(`Unknown action: ${action}: ${action.actionArg}`)    
+                }
+                console.log(`Calling function ${action} with argument ${actionArg}`)
+                const observation = await availableFunctions[action](actionArg)
+                messages.push({ role: "assistant", content: `Observation: ${ observation }` })
+            } else {
+                console.log("Agent finished with task")
+                return responseText
+            }
+        }
 }
 
-agent("What book should I read next? I like self-help books.")
+console.log(await agent("What is the current weather in my location?"))
 
